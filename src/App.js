2c0bf36d90f9f2ba2682d616bed6e77abc79cd44
Shivneel-Prasad <Shivam201020@gmail.com>
2021-12-09 09:50:05 -0800

    Compiled Successfully - Assignment Complete

--- src/App.js
+++ src/App.js
@@ -7,27 +7,38 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+import { CartContext } from './contexts/cartContext';
+import { ProductContext } from './contexts/productContext';
+
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
 		// add the given item to the cart
+		return setCart([
+			...cart,
+			item
+		])
 	};
 
 	return (
-		<div className="App">
-			<Navigation cart={cart} />
+	  <div className="App">
+		<CartContext.Provider value={{cart}}>
+	      <Navigation />
 
 			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
+			<ProductContext.Provider value={{ products, addItem }}>
+			  <Route exact path="/">
+				<Products />
+			  </Route>
+			</ProductContext.Provider>
 
 			<Route path="/cart">
-				<ShoppingCart cart={cart} />
+				<ShoppingCart />
 			</Route>
-		</div>
+		</CartContext.Provider>
+	  </div>
 	);
 }
 
